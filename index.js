9dd0cf980cb7ce112524a5fa1147116007128820
osoTripolar <omar.trafalgar.2001@gmail.com>
2025-08-01 13:46:30 -0500

    pg added

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import {config} from 'dotenv';
+import pg from 'pg';
+
+config();
+
+const PORT = 3000
 
 const app = express ()
-const PORT = 3000
+const pool = new pg.Pool({
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
+})
+
+app.get('/ping', async (req,res)=>{
+    const resul = await pool.query('SELECT NOW()')
+    return res.json(resul.rows[0])
+})
 
 app.get('/', (req,res)=>{
     res.send('Bienvenido a la página de Osito Style')
 })
 
-app.listen(PORT)
-console.log('Server on port', PORT)
+app.listen(PORT, () => {
+    console.log('Server on port', PORT)
+}) 
