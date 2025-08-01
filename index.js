80e9e198be646fee5171e18aa5ec333ad2d73c8a
osoTripolar <omar.trafalgar.2001@gmail.com>
2025-08-01 17:30:57 -0500

    probando SSL dinamico

--- index.js
+++ index.js
@@ -2,16 +2,45 @@ import express from 'express';
 import {config} from 'dotenv';
 import pg from 'pg';
 
-config();
+config(); //nos permite trabajar con variables de entorno
 
-const PORT = 3000
+const PORT = 3000 // puerto por defecto
 
 const app = express ()
-const pool = new pg.Pool({
+
+/////// CONEXION A BASE DE DATOS
+
+const resLocal = { 
     connectionString : process.env.DATABASE_URL,
-    //PARA TRABAJAR DE MANERA LOCAL: REQUIERE SSL EN TRUE
-    // ssl: true
-})
+    ssl: true
+
+    } //PARA TRABAJAR DE MANERA LOCAL: REQUIERE SSL EN TRUE
+
+const resServer = {
+    connectionString : process.env.DATABASE_URL,
+    }//PARA SUBIR A RENDER: NO DEBE LLEVAR SSL
+
+let respuestaJSON = resLocal
+    
+console.log(respuestaJSON, 'ENVIRONTMENT', process.env.ENVIRONTMENT)
+// HACER UNA FUNCION QUE DEPENDIENDO DE SI ESTAMOS EN LOCAL O EN PRODUCCION ACTIVE O DESACTIVE EL SS
+
+if (!(process.env.ENVIRONTMENT === 'development')){
+    respuestaJSON = resServer
+    console.log('Estamos en produccion')
+} else{    
+    console.log('Estamos en desarrollo')
+}
+
+
+
+
+
+const pool = new pg.Pool(respuestaJSON) // tengo k pegarle dentro la respuesta
+
+/////// RUTAS
+
+
 
 app.get('/ping', async (req,res)=>{
     const resul = await pool.query('SELECT NOW()')
